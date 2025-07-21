d79fd984cce81d9cde2a087defa95a9056329bea
RAFFY PERALTA <raffy.peralta.01@gmail.com>
2025-07-22 00:00:13 +0800

    add imports, controllers and provider

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ActivityService } from './activity/activity.service';
+import { ActivityModule } from './activity/activity.module';
+import { RecordModule } from './record/record.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ActivityModule, RecordModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ActivityService],
 })
 export class AppModule {}
